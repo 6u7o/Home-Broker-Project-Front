7f28ff38da49c237e8c5393d52e5f12213972fdb
6u7o <augustopinheiro19999999@gmail.com>
2022-09-11 22:54:22 -0300

    FEAT: creates api instance

--- src/API/index.js
+++ src/API/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:${process.env.REACT_APP_BACKEND_PORT || '3000'}`,
+  baseURL: `http://localhost:${process.env.PORT || '3000'}`,
 });
 
 export default api;
