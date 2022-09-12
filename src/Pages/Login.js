33aa3c82b7b38ec16ec3bd39d5ed2d22e6f1304a
6u7o <augustopinheiro19999999@gmail.com>
2022-09-11 22:56:19 -0300

    FEAT: creates Login page

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import logo from '../images/stockLogo.png';
 import api from '../API';
+import './styles/Login.css';
 
 function Login() {
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(false);
@@ -14,42 +19,55 @@ function Login() {
       localStorage.setItem('email', email);
       localStorage.setItem('lastLogin', Date.now());
       localStorage.setItem('token', token);
-      console.log('deu!');
+      const config = {
+        headers: {
+          authorization: token,
+        },
+      };
+      const { data } = await api.get('/conta', config);
+      localStorage.setItem('userId', data.id);
+      navigate('/home');
     } else {
       setError(true);
     }
   };
 
   return (
-    <form>
-        <label htmlFor="email">
-          <input
+    <div className="login">
+      <Form className="login-box">
+        <div className="logo-box">
+          <img className="logo" src={ logo } alt="logo do app" />
+        </div>
+        <Form.Label htmlFor="email">
+          <Form.Control
             type="text"
             aria-label="email"
             onChange={ ({ target }) => setEmail(target.value) }
             value={ email }
           />
-        </label>
-        <label htmlFor="password">
-          <input
+        </Form.Label>
+        <Form.Label htmlFor="password">
+          <Form.Control
             type="password"
             aria-label="password"
             onChange={ ({ target }) => setPassword(target.value) }
             value={ password }
           />
-        </label>
-        <button
+        </Form.Label>
+        <Button
           type="submit"
           aria-label="login-button"
           onClick={ onClickButton }
+          className="login-button"
         >
           LOGIN
-        </button>
+        </Button>
         {error && (
           <span>
             Email ou senha inválidos
           </span>)}
-      </form>
+      </Form>
+    </div>
   );
 }
 
