0c0d3240c57bad7313c364d6cab8a7823d78234c
6u7o <augustopinheiro19999999@gmail.com>
2022-09-11 22:54:59 -0300

    FEAT: creates routes for the application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Pages/Login';
+import Stocks from './Components/Stocks';
+import Account from './Components/Account';
+import UserAssets from './Components/UserAssets';
+import Dashboard from './Pages/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={ <Login /> } />
+        <Route path="/stocks" element={ <Stocks /> } />
+        <Route path="/account" element={ <Account /> } />
+        <Route path="/home" element={ <Dashboard /> } />
+        <Route path="/userAssets" element={ <UserAssets /> } />
         <Route exact path="/" element={ <Navigate to="/login" /> } />
       </Routes>
     </BrowserRouter>
