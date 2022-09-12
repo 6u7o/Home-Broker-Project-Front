1bff4530f554fa7765d17f711c6445f3b68fa139
6u7o <augustopinheiro19999999@gmail.com>
2022-09-12 14:00:33 -0300

    REFAC: change routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Pages/Login';
-import Stocks from './Components/Stocks';
-import Account from './Components/Account';
-import UserAssets from './Components/UserAssets';
+// import Stocks from './Components/Stocks';
+// import Account from './Components/Account';
+// import UserAssets from './Components/UserAssets';
 import Dashboard from './Pages/Dashboard';
+import AccountPage from './Pages/AccountPage';
+import StocksPage from './Pages/StocksPage';
+import UserAssetsPage from './Pages/UserAssetsPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={ <Login /> } />
-        <Route path="/stocks" element={ <Stocks /> } />
+        {/* <Route path="/stocks" element={ <Stocks /> } />
         <Route path="/account" element={ <Account /> } />
+        <Route path="/userAssets" element={ <UserAssets /> } /> */}
         <Route path="/home" element={ <Dashboard /> } />
-        <Route path="/userAssets" element={ <UserAssets /> } />
+        <Route path="/conta" element={ <AccountPage /> } />
+        <Route path="/ativos" element={ <StocksPage /> } />
+        <Route path="/meus-ativos" element={ <UserAssetsPage /> } />
         <Route exact path="/" element={ <Navigate to="/login" /> } />
       </Routes>
     </BrowserRouter>
